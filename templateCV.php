bf2906ec22d70e166ffa859d0bc82afba87f8e6f
Thomasevano <thomas-evano@hotmail.fr>
2016-12-04 22:57:31 +0100

    fix

--- templateCV.php
+++ templateCV.php
@@ -13,11 +13,11 @@ session_start();
     <div class="base">
         <h1>Choisissez votre de template</h1>
         <h6>Cliquez sur l'image afin de générer ce template en PDF</h6>
-        <a href="CVPDF1.php"><img src="img/Resume_Template1.jpg" class="template_preview"></a>
-        <a href="CVPDF2.php"><img src="img/Resume_Template2.jpg" class="template_preview"></a>
-        <a href="CVPDF3.php"><img src="img/Resume_Template4.jpg" class="template_preview"></a>
-        <a href="CVPDF4.php"><img src="img/Resume_Template5.jpg" class="template_preview"></a>
-        <a href="CVPDF5.php"><img src="img/Resume_Template6.jpg" class="template_preview"></a>
+        <a href="CVPDF1.php"><img src="img/Resume_Template1.1.jpg" class="template_preview"></a>
+        <a href="CVPDF2.php"><img src="img/Resume_Template2.2.jpg" class="template_preview"></a>
+        <a href="CVPDF3.php"><img src="img/Resume_Template3.3.jpg" class="template_preview"></a>
+        <a href="CVPDF4.php"><img src="img/Resume_Template4.4.jpg" class="template_preview"></a>
+        <a href="CVPDF5.php"><img src="img/Resume_Template5.5.jpg" class="template_preview"></a>
     </div>
 </div>
 </body>
